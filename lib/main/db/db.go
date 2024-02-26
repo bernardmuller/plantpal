e269ab91248f26861f5a361b4b2cf9ddbe0fa5c2
Bernard Muller <b.mullerjnr@gmail.com>
2024-02-26 15:30:01 +0200

    env vars

--- lib/main/db/db.go
+++ lib/main/db/db.go
@@ -3,20 +3,20 @@ package db
 import (
 	"database/sql"
 	"fmt"
+  "os"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func connect_db() (*sql.DB, error) {
-  // database_name := os.Getenv("TEST_DATABASE_URL")
-  // auth_token := os.Getenv("TURSO_AUTH_TOKEN")
-  //
-  // if database_name == "" || auth_token == "" {
-  //   return nil, fmt.Errorf("DATABASE_NAME and AUTH_TOKEN environment variables must be set")
-  // }
+  database_name := os.Getenv("TEST_DATABASE_URL")
+  auth_token := os.Getenv("TURSO_AUTH_TOKEN")
 
-  url := "libsql://domain-db-test-bernardmuller.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDg5MzA3OTMsImlkIjoiNTQxOTZhYjgtZDQ3My0xMWVlLWIzMmEtZGU0YWQ3NTljZGZlIn0.HEKTJ5RbybF8ckh4KfRBgImE7Al-yRw7YU-ONUX8Ig7mwnYpUihlQrX3FCEDZVQZQkM4TDdugDzFHPqqxT3UBw"
-  // url := fmt.Sprintf("[%s]?authToken=%s", database_name, auth_token)
+  if database_name == "" || auth_token == "" {
+    return nil, fmt.Errorf("DATABASE_NAME and AUTH_TOKEN environment variables must be set")
+  }
+
+  url := fmt.Sprintf("[%s]?authToken=%s", database_name, auth_token)
 
   db, err := sql.Open("libsql", url)
   if err != nil {
