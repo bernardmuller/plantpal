11d786cd29c972afe783dbf3a9a2c32d17eaf12a
Bernard Muller <b.mullerjnr@gmail.com>
2024-02-26 16:58:47 +0200

    env vars

--- lib/main/db/db.go
+++ lib/main/db/db.go
@@ -3,7 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
-  "os"
+	"os"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
@@ -11,6 +11,9 @@ import (
 func connect_db() (*sql.DB, error) {
   database_name := os.Getenv("TEST_DATABASE_URL")
   auth_token := os.Getenv("TURSO_AUTH_TOKEN")
+
+  fmt.Printf("%s" , database_name)
+  fmt.Printf("%s" , auth_token)
 
   if database_name == "" || auth_token == "" {
     return nil, fmt.Errorf("DATABASE_NAME and AUTH_TOKEN environment variables must be set")
