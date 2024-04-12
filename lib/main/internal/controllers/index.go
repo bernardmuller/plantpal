3a62421fdac23a9865c390b14fea5c530e216a88
bernardmuller <b.mullerjnr@gmail.com>
2024-04-12 05:39:09 +0200

    fix: build errors

--- lib/main/internal/controllers/index.go
+++ lib/main/internal/controllers/index.go
@@ -1,15 +1,16 @@
 package controllers
 
-import "domain-app/internal/config"
-
-type Controllers struct {
-	Plants *PlantsController
-}
-
-func NewControllers(ac config.ApiConfig) Controllers {
-	return Controllers{
-		Plants: &PlantsController{
-			config: &ac,
-		},
-	}
-}
+//
+//import "domain-app/internal/config"
+//
+//type Controllers struct {
+//	Plants *PlantsController
+//}
+//
+//func NewControllers(ac config.ApiConfig) Controllers {
+//	return Controllers{
+//		Plants: &PlantsController{
+//			config: &ac,
+//		},
+//	}
+//}
