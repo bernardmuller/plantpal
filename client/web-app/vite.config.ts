02951189bc9d3f5af91bd56751e68f2937346d07
bernardmuller <b.mullerjnr@gmail.com>
2024-06-24 08:47:27 +0200

    feat: plantpal init

--- client/web-app/vite.config.ts
+++ client/web-app/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	base: "/",
-	plugins: [react()],
 	preview: {
 		port: 8000,
 		strictPort: true,
@@ -14,4 +14,56 @@ export default defineConfig({
 		host: true,
 		origin: "http://0.0.0.0:8000",
 	},
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "prompt",
+			devOptions: {
+				enabled: true,
+			},
+			injectRegister: "auto",
+			includeAssets: [
+				"favicon.ico",
+				"apple-touch-icon.png",
+				"maskable_icon.png",
+			],
+			manifest: {
+				name: "PlantPal",
+				short_name: "PlantPal",
+				description: "PlantPal - Your plant companion app",
+				theme_color: "#171717",
+				background_color: "#f0e7db",
+				display: "standalone",
+				scope: "/",
+				start_url: "/",
+				orientation: "portrait",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "favicon",
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "favicon",
+					},
+					{
+						src: "/apple-touch-icon.png",
+						sizes: "180x180",
+						type: "image/png",
+						purpose: "apple touch icon",
+					},
+					{
+						src: "/maskable_icon.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+		}),
+	],
 });
